346915066ce12247f29e9f237a838992f16e68b2
ritikjain6521 <168226242+ritikjain6521@users.noreply.github.com>
2024-07-14 13:05:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['body-parser'],
-    },
-  },
-});
+  plugins: [react()],
+})
 
